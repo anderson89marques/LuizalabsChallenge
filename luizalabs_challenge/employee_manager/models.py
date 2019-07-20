3bb18b38d3afe93cd17a8479e61df80f16c02a69
AndersonMarquesGeru <anderson.morais@geru.com.br>
2019-07-20 20:01:38 -0300

    Add Employee model

--- luizalabs_challenge/employee_manager/models.py
+++ luizalabs_challenge/employee_manager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    department = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
